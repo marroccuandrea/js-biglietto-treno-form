bf04d89e4a2a6a392330b8f46f61b2538bf50ae6
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-21 18:14:56 +0100

    [add] calculation code (not working)

--- js/script.js
+++ js/script.js
@@ -1,21 +1,50 @@
 //Input
-const inputNome = document.querySelector('.input-nome');
-const inputKm = document.querySelector('.input-km');
-const selectAge = document.querySelector('.select-age');
+let inputNome = document.querySelector('.input-nome');
+let inputKm = document.querySelector('.input-km');
+let selectAge = document.querySelector('.select-age');
 
-console.log(inputNome);
+//Costant
+const priceKm = 0.21;
+let minorenne = document.querySelector('.minorenne');
+let maggiorenne = document.querySelector('.maggiorenne');
+let anziano = document.querySelector('.anziano');
+
+
+
 
 //Buttons
 const btn1 = document.querySelector('.btn1');
 const btn2 = document.querySelector('.btn2');
-console.log(btn1);
+
 
 btn1.addEventListener('click', function(){
-    const nome = inputNome.value;
-    const km = inputKm.value;
-    const eta = selectAge.value;
-
-    console.log(nome, km, eta);
+    let nome = inputNome.value;
+    let km = inputKm.value;
+    let eta = selectAge.value;
+    let price = parseFloat((km * priceKm).toFixed(2));
+    let underDisc = ((price * 20) /100);
+    let overDisc = ((price * 40) /100);
+    const priceDiscUnder = price - underDisc;
+    const priceDiscOver = price - overDisc;
+    // console.log(nome, km, eta, priceDiscOver);
+    if (eta == minorenne) {
+        console.log('Applico sconto 20%');
+        
+        console.log(priceDiscUnder);
+        document.getElementById('output').innerHTML = 'Hai diritto allo sconto del 20% !'
+    }
+    else if (eta == anziano){
+        console.log('Applico sconto del 40%');
+        underDisc;
+        console.log(priceDiscOver);
+        document.getElementById('output').innerHTML = 'Hai diritto allo sconto del 40% !'
+    }
+    else if (eta == maggiorenne){
+        console.log('Nessuno sconto applicato');
+        underDisc = 0;
+        console.log(price);
+    }
+    
 })
 
 btn2.addEventListener('click', function(){
