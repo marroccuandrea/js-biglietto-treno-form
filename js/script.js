e5775d8de4dd25c13bb40fdae40b1a370944c2fb
marroccuandrea <marroccuandrea@yahoo.it>
2024-02-21 16:10:36 +0100

    [add] const input & buton

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 //Input
+const inputNome = document.querySelector('.input-nome');
+const inputKm = document.querySelector('.input-km');
+const selectAge = document.querySelector('select-age');
+
+console.log(inputNome);
+
+//Buttons
+const btn1 = document.querySelector('btn1');
+const btn2 = document.querySelector('btn2');
